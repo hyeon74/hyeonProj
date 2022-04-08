078167eff58936181990b8a52dcdc2c19d14dc7b
hyeon <hyeon@hyeonui-MacBookPro.local>
2022-04-08 20:57:45 +0900

    new file:   nuxtsrc/assets/variables.scss 	new file:   nuxtsrc/components/NuxtLogo.vue 	new file:   nuxtsrc/components/SubTabMenu01.vue 	new file:   nuxtsrc/layouts/default.vue 	new file:   nuxtsrc/layouts/error.vue 	new file:   nuxtsrc/nuxt.config.js 	new file:   nuxtsrc/package.json 	new file:   nuxtsrc/pages/index.vue 	new file:   nuxtsrc/pages/inspire.vue 	new file:   nuxtsrc/pages/mixgas01.vue 	new file:   nuxtsrc/pages/mixgas02.vue 	new file:   nuxtsrc/pages/mixgas03.vue 	new file:   nuxtsrc/static/favicon.ico 	new file:   nuxtsrc/yarn.lock

---
+++ nuxtsrc/pages/mixgas01.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>MixGas01</h1>
+    <SubTabMenu01 />
+    {{pO2Cal}}
+  </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return{
+            Dil_O2_Val: 21,
+            Dil_N2_Val: 78,
+            Dil_He_Val,
+            Bailout_O2_Val,
+            Bailout_N2_Val,
+            Bailout_He_Val,
+            Set_DEPTH_Val,
+            Set_END_Val,
+            Setpoint_Val,
+        }
+    },
+    computed: {
+      //수심에 대한 절대압
+      depAta_Cal() {
+        return 'depAta_Cal';
+      },
+      //END에 대한 절대값
+      endAta_Cal() {
+        return 'endAta_Cal';
+      },
+      //산소 부분압
+      ppO2_Cal() {
+        return 'pO2Cal';
+      },
+      //산소 비율
+      fO2_Cal() {
+        return 'fO2_Cal';
+      },
+      //질소 부분압
+      ppN2_Cal() {
+        return 'ppN2_Cal';
+      },
+      //질소 비율
+      fN2_Cal() {
+        return 'fN2_Cal';
+      },
+      //헬륨 부분압
+      ppHe_Cal() {
+        return 'ppHe_Cal';
+      },
+      //헬륨 비율
+      fHe_Cal() {
+        return 'fHe_Cal';
+      }
+
+    },
+    methods: {
+        
+    }
+}
+</script>
